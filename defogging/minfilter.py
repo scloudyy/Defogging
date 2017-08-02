150718df02aab0670565b74f853ebd5b4987f0a2
scloudyy <onecloud.shen@gmail.com>
2017-08-02 23:08:07 +0800

    feat: add minfilter

---
+++ defogging/minfilter.py
@@ -0,0 +1,18 @@
+from numpy import *
+from utils import padding
+
+
+def minfilter(src, r):
+    """
+
+    :param src: input(one channel only)
+    :param r: window radius
+    :return: dst
+    """
+    (hei, wid) = src.shape[0:2]
+    dst = zeros((hei, wid))
+    src_pad = padding(src, r)
+    for i in range(hei):
+        for j in range(wid):
+            dst[i, j] = amin(src_pad[i:i+2*r, j:j+2*r])
+    return dst
