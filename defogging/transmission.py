b490422a8e77e860be57b062816c3a6c1f05e22f
scloudyy <onecloud.shen@gmail.com>
2017-08-03 12:37:47 +0800

    feat: add guidedfilter

--- defogging/transmission.py
+++ defogging/transmission.py
@@ -1,12 +1,13 @@
 from numpy import *
 from minfilter import minfilter
 
-def transmission(src, A, r):
+def transmission(src, A, r, w):
     """
 
     :param src: original input image(three channels)
     :param A: airlight(1,1,3)
     :param r: radius of darkchannel
+    :param w: t = 1 - w*(I/A)
     :return: dst
     """
     (hei, wid) = src.shape[0:2]
